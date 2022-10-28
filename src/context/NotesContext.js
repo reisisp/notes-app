59f6faa639d8c0ab5fb74a36f962b3aa23b4e95a
Alex <vuyanalex@gmail.com>
2022-10-28 16:45:24 +0300

    feature: add crud Context logic

--- src/context/NotesContext.js
+++ src/context/NotesContext.js
@@ -1,14 +1,67 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { createContext, useState } from 'react';
+import NotesService from '../services/notes-service';
 
 const NotesContext = createContext();
+const service = new NotesService()
 
 
 export function NotesContextProvider({ children }) {
     const [notesList, setNotesList] = useState(null);
 
+    function getNotesList() {
+        service
+            .getNotesList()
+            .then((res) => {
+                setNotesList(res)
+            })
+            .catch(() => {
+                console.log('Smth wrong')
+            })
+    }
+
+    function addNoteItem(title) {
+        service
+            .addNewNote(title)
+            .then(() => {
+                getNotesList()
+            })
+            .catch(() => {
+                console.log('Smth wrong')
+            })
+    }
+
+    function removeNoteItem(id) {
+        service
+            .removeNoteByID(id)
+            .then(() => {
+                getNotesList()
+            })
+            .catch(() => {
+                console.log('Smth wrong')
+            })
+    }
+
+
+    function updateNoteItem(id, obj) {
+        service
+            .updateNoteById(id, obj)
+            .then(() => {
+                getNotesList()
+            })
+            .catch(() => {
+                console.log('Smth wrong')
+            })
+    }
+
+
+    useEffect(() => {
+        getNotesList()
+    }, [])
+
     return (
-        <NotesContext.Provider value={{ notesList }}>{children}</NotesContext.Provider>
+        <NotesContext.Provider value={{ notesList, addNoteItem, removeNoteItem, updateNoteItem }}>{children}</NotesContext.Provider>
     )
 }
 
