b5c66355df8539405c610ddfeb82e5d82c398176
Alex <vuyanalex@gmail.com>
2022-10-28 18:36:22 +0300

    fix: added basic styles for NoteList & SideBar & NoteCard

--- src/components/NoteList/NoteList.js
+++ src/components/NoteList/NoteList.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import NoteItem from "../NoteItem";
 import { List } from 'antd';
 import NotesContext from "../../context/NotesContext";
+import classes from './NoteList.module.scss'
 
 export const NoteList = () => {
     const { notesList } = useContext(NotesContext)
@@ -10,7 +11,7 @@ export const NoteList = () => {
             itemLayout="horizontal"
         >
             {notesList.map(noteItem => (
-                <List.Item key={noteItem.id}>
+                <List.Item className={classes.list__item} key={noteItem.id}>
                     <NoteItem note={noteItem} />
                 </List.Item>
             ))}
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Layout } from 'antd';
 import NoteList from "../NoteList";
+import classes from './SideBar.module.scss'
 
 const { Sider } = Layout;
 
 export const Sidebar = () => {
     return (
-        <Sider>
+        <Sider className={classes.sidebar}>
             <NoteList />
         </Sider>
     )
