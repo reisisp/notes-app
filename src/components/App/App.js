78b29369e9d82ed790725f38a18f126e8a317257
Alex <vuyanalex@gmail.com>
2022-10-28 17:16:34 +0300

    fix: added atnd components to NotesPage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NotesContextProvider } from "../../context/NotesContext";
 import NotesPage from "../NotesPage";
+import 'antd/dist/antd.min.css';
 
 export const App = () => {
     return (
--- src/components/NotesPage/NotesPage.js
+++ src/components/NotesPage/NotesPage.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { useContext } from "react";
+import { Layout } from 'antd';
+import classes from './NotesPage.module.scss'
+
 import NotesContext from "../../context/NotesContext";
+
+const { Header, Footer, Sider, Content } = Layout;
+
 
 export const NotesPage = () => {
     const { notesList } = useContext(NotesContext)
-    console.log(notesList)
+
     return (
-        <p>notes</p>
+        <Layout className={classes.container}>
+            <Sider>Side</Sider>
+            <Layout>
+                <Header>Head</Header>
+                <Content>Content</Content>
+                <Footer>Footer</Footer>
+            </Layout>
+        </Layout>
     )
 }
